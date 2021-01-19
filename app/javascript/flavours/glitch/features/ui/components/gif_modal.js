4d57353991c10ad581d32f17d7c79ada9c8976cf
koyu <me@koyu.space>
2021-01-19 17:51:44 +0100

    Add translations, close button and make Giphy modal more beautiful

--- app/javascript/flavours/glitch/features/ui/components/gif_modal.js
+++ app/javascript/flavours/glitch/features/ui/components/gif_modal.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from 'flavours/glitch/components/button';
 import ImmutablePureComponent from 'react-immutable-pure-component';
 import { connect } from 'react-redux';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import { giphySet, uploadCompose } from 'flavours/glitch/actions/compose';
 import IconButton from 'flavours/glitch/components/icon_button';
-import { debounce, mapValues } from 'lodash';
-import classNames from 'classnames';
 import ReactGiphySearchbox from 'react-giphy-searchbox'
-import { changeCompose } from 'flavours/glitch/actions/compose';
+import { defineMessages, injectIntl } from 'react-intl';
+
+const messages = defineMessages({
+  search:    { id: 'giphy.search', defaultMessage: 'Search for GIFs' },
+  error:     { id: 'giphy.error', defaultMessage: 'Oops! Something went wrong. Please, try again.' },
+  loading:   { id: 'giphy.loading', defaultMessage: 'Loading...'},
+  nomatches: { id: 'giphy.nomatches', defaultMessage: 'No matches found.' },
+  close:     { id: 'settings.close', defaultMessage: 'Close' },
+});
 
 // Utility for converting base64 image to binary for upload
 // https://stackoverflow.com/questions/35940290/how-to-convert-base64-string-to-javascript-file-object-like-as-from-file-input-f
@@ -34,18 +39,16 @@ const mapDispatchToProps = dispatch => ({
 });
 
 export default @connect(mapStateToProps, mapDispatchToProps)
+@injectIntl
 class GIFModal extends ImmutablePureComponent {
 
   static propTypes = {
+    intl: PropTypes.object.isRequired,
     options: ImmutablePropTypes.map,
     onClose: PropTypes.func.isRequired,
     setOpt: PropTypes.func.isRequired,
     submit: PropTypes.func.isRequired,
   };
-
-  componentDidMount() {
-    //If component mounted
-  }
 
   onDoneButton = (item) => {
     const url = item["images"]["original"]["mp4"];
@@ -64,31 +67,14 @@ class GIFModal extends ImmutablePureComponent {
     });
   };
 
-  handleClick = () => {
-    this.props.onClose();
-  }
-
-  handleCancel = () => {
-    this.props.onClose();
-  }
-
-  setRef = (c) => {
-    this.button = c;
-  }
-
-  toggleNotifications = () => {
-    this.props.onToggleNotifications();
-  }
-
-  changeMuteDuration = (e) => {
-    this.props.onChangeMuteDuration(e);
-  }
-
   render () {
 
+    const { intl } = this.props;
+
     return (
-      <div className='modal-root__modal mute-modal'>
-        <div className='mute-modal__container'>
+      <div className='modal-root__modal giphy-modal'>
+        <div className='giphy-modal__container'>
+          <IconButton title={intl.formatMessage(messages.close)} icon="close" size="16" onClick={this.props.onClose}  style={{float: "right"}} /><br />
           <ReactGiphySearchbox
             apiKey="1ttK05MF98dLllFFknTAVo0U4CGcQb4J"
             onSelect={item => this.onDoneButton(item)}
@@ -96,6 +82,12 @@ class GIFModal extends ImmutablePureComponent {
               { columns: 2, imageWidth: 190, gutter: 5 },
               { mq: "700px", columns: 2, imageWidth: 210, gutter: 5 }
             ]}
+            autoFocus="true"
+            searchPlaceholder={intl.formatMessage(messages.search)}
+            messageError={intl.formatMessage(messages.error)}
+            messageLoading={intl.formatMessage(messages.loading)}
+            messageNoMatches={intl.formatMessage(messages.nomatches)}
+            wrapperClassName="giphy-modal__searchbox"
           />
         </div>
       </div>
