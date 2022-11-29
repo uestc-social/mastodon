d249994de839d60d2fa96b6510558c080315f1f5
fef <owo@fef.moe>
2022-11-29 08:54:35 +0000

    cleanup frontend emoji reaction code

--- app/javascript/flavours/glitch/selectors/index.js
+++ app/javascript/flavours/glitch/selectors/index.js
@@ -137,3 +137,11 @@ export const getAccountHidden = createSelector([
 ], (hidden, followingOrRequested, isSelf) => {
   return hidden && !(isSelf || followingOrRequested);
 });
+
+export const makeCustomEmojiMap = createSelector(
+  [state => state.get('custom_emojis')],
+  items => items.reduce(
+    (map, emoji) => map.set(emoji.get('shortcode'), emoji),
+    ImmutableMap(),
+  ),
+);
