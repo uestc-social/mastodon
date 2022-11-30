c3a18c6fffb2ed5ce7b6fbee6b1a4ee8c42a6f28
fef <owo@fef.moe>
2022-11-30 13:20:20 +0000

    make number of displayed reactions a setting
    This adds an extra item to the local settings for
    specifying the number of reactions shown in toots.
    The detailed status view always shows all
    reactions.

---
+++ app/javascript/flavours/glitch/locales/fr.js
@@ -0,0 +1,15 @@
+import inherited from 'mastodon/locales/fr.json';
+
+const messages = {
+  'notification.reaction': '{name} a réagi·e à votre message',
+  'notifications.column_settings.reaction': 'Réactions:',
+
+  'tooltips.reactions': 'Réactions',
+
+  'settings.enter_amount_prompt': 'Entrez un montant',
+  'settings.num_visible_reactions': 'Nombre de réactions visibles',
+
+  'status.react': 'Réagir',
+};
+
+export default Object.assign({}, inherited, messages);
