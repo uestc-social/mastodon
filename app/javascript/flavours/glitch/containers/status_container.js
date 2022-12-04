76a519f83eee4851b1779e1ce67cc70955554662
fef <owo@fef.moe>
2022-12-04 12:33:47 +0000

    properly disable reactions when not logged in

--- app/javascript/flavours/glitch/containers/status_container.js
+++ app/javascript/flavours/glitch/containers/status_container.js
@@ -166,11 +166,7 @@ const mapDispatchToProps = (dispatch, { intl, contextType }) => ({
   },
 
   onReactionAdd (statusId, name, url) {
-    const { signedIn } = this.context.identity;
-
-    if (signedIn) {
-      dispatch(addReaction(statusId, name, url));
-    }
+    dispatch(addReaction(statusId, name, url));
   },
 
   onReactionRemove (statusId, name) {
--- app/javascript/flavours/glitch/features/status/components/detailed_status.js
+++ app/javascript/flavours/glitch/features/status/components/detailed_status.js
@@ -27,6 +27,7 @@ class DetailedStatus extends ImmutablePureComponent {
 
   static contextTypes = {
     router: PropTypes.object,
+    identity: PropTypes.object,
   };
 
   static propTypes = {
@@ -330,6 +331,7 @@ class DetailedStatus extends ImmutablePureComponent {
             reactions={status.get('reactions')}
             addReaction={this.props.onReactionAdd}
             removeReaction={this.props.onReactionRemove}
+            canReact={this.context.identity.signedIn}
           />
 
           <div className='detailed-status__meta'>
