2bf445d1e457210de8332194ff88f120b5f5a076
koyu <me@koyu.space>
2023-01-22 13:16:28 +0100

    Fixing some errors

--- app/javascript/flavours/glitch/features/ui/components/gif_modal.js
+++ app/javascript/flavours/glitch/features/ui/components/gif_modal.js
@@ -76,7 +76,7 @@ class GIFModal extends ImmutablePureComponent {
     return (
       <div className='modal-root__modal tenor-modal'>
         <div className='tenor-modal__container'>
-        <IconButton title={intl.formatMessage(messages.close)} icon='close' size="16" onClick={this.props.onClose}  style={{ float: 'right' }} />
+          <IconButton title={intl.formatMessage(messages.close)} icon='close' size='16' onClick={this.props.onClose}  style={{ float: 'right' }} />
           <Tenor
             token='FJBKNQSVF2DD'
             // eslint-disable-next-line react/jsx-no-bind
