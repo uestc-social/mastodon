f577764472c786d374e1b0510e71114277752114
neatchee <neatchee@gmail.com>
2023-01-26 11:32:03 -0800

    Fix rebase issues

--- app/javascript/flavours/glitch/components/status_action_bar.jsx
+++ app/javascript/flavours/glitch/components/status_action_bar.jsx
@@ -331,7 +331,7 @@ class StatusActionBar extends ImmutablePureComponent {
         <IconButton className={classNames('status__action-bar-button', { reblogPrivate })} disabled={!publicStatus && !reblogPrivate} active={status.get('reblogged')} title={reblogTitle} icon={reblogIcon} onClick={this.handleReblogClick} counter={withCounters ? status.get('reblogs_count') : undefined} />
         <IconButton className='status__action-bar-button star-icon' animate active={status.get('favourited')} title={intl.formatMessage(messages.favourite)} icon='star' onClick={this.handleFavouriteClick} counter={withCounters ? status.get('favourites_count') : undefined} />
         {
-          signedIn
+          permissions
             ? <EmojiPickerDropdown className='status__action-bar-button' onPickEmoji={this.handleEmojiPick} button={reactButton} disabled={!canReact} />
             : reactButton
         }
