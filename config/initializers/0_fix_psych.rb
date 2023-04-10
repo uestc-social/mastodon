445f81cbbbab13ddf858cb829db7465ac838f2f9
Essem <smswessem@gmail.com>
2023-04-10 15:58:22 -0500

    Remove psych fix

--- config/initializers/0_fix_psych.rb
+++
@@ -1,9 +0,0 @@
-# Ruby 3.1 includes a breaking update to Psych 4.
-# Undo the breakage, as Mastodon doesn't load untrusted YAML files.
-# https://stackoverflow.com/a/71192990
-
-module YAML
-  class << self
-    alias_method :load, :unsafe_load if YAML.respond_to? :unsafe_load
-  end
-end
