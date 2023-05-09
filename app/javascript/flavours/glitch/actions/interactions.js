13657de04e9c95299c91ecc9146450d31b4f8465
Jeremy Kescher <jeremy@kescher.at>
2023-05-10 00:11:15 +0200

    eslint fix

--- app/javascript/flavours/glitch/actions/interactions.js
+++ app/javascript/flavours/glitch/actions/interactions.js
@@ -402,7 +402,7 @@ export function unpinFail(status, error) {
     status,
     error,
   };
-};
+}
 
 export const addReaction = (statusId, name, url) => (dispatch, getState) => {
   const status = getState().get('statuses').get(statusId);
