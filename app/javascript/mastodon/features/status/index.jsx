4ba93c2c10c805f0a3afdd48f271734e9937aba4
Jeremy Kescher <jeremy@kescher.at>
2023-05-10 00:05:39 +0200

    Remove further leftover makeCustomEmojiMap references

--- app/javascript/mastodon/features/status/index.jsx
+++ app/javascript/mastodon/features/status/index.jsx
@@ -49,7 +49,7 @@ import { initMuteModal } from '../../actions/mutes';
 import { initBlockModal } from '../../actions/blocks';
 import { initBoostModal } from '../../actions/boosts';
 import { initReport } from '../../actions/reports';
-import { makeCustomEmojiMap, makeGetStatus, makeGetPictureInPicture } from '../../selectors';
+import { makeGetStatus, makeGetPictureInPicture } from '../../selectors';
 import ScrollContainer from 'mastodon/containers/scroll_container';
 import ColumnHeader from '../../components/column_header';
 import StatusContainer from '../../containers/status_container';
@@ -63,7 +63,6 @@ import { textForScreenReader, defaultMediaVisibility } from '../../components/st
 import { Icon }  from 'mastodon/components/icon';
 import { Helmet } from 'react-helmet';
 import BundleColumnError from 'mastodon/features/ui/components/bundle_column_error';
-import { makeCustomEmojiMap } from '../../selectors';
 
 const messages = defineMessages({
   deleteConfirm: { id: 'confirmations.delete.confirm', defaultMessage: 'Delete' },
@@ -156,7 +155,6 @@ const makeMapStateToProps = () => {
       askReplyConfirmation: state.getIn(['compose', 'text']).trim().length !== 0,
       domain: state.getIn(['meta', 'domain']),
       pictureInPicture: getPictureInPicture(state, { id: props.params.statusId }),
-      emojiMap: makeCustomEmojiMap(state),
     };
   };
 
@@ -264,11 +262,11 @@ class Status extends ImmutablePureComponent {
     if (signedIn) {
       dispatch(addReaction(statusId, name, url));
     }
-  }
+  };
 
   handleReactionRemove = (statusId, name) => {
     this.props.dispatch(removeReaction(statusId, name));
-  }
+  };
 
   handlePin = (status) => {
     if (status.get('pinned')) {
