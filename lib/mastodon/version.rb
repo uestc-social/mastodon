e036958642852f16e2e1149666ee172f0c5bafe2
Essem <smswessem@gmail.com>
2023-07-17 10:26:33 -0500

    Point to Chuckya repo URL by default

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -33,7 +33,7 @@ module Mastodon
     end
 
     def repository
-      ENV.fetch('GITHUB_REPOSITORY', 'glitch-soc/mastodon')
+      ENV.fetch('GITHUB_REPOSITORY', 'TheEssem/mastodon')
     end
 
     def source_base_url
--- spec/presenters/instance_presenter_spec.rb
+++ spec/presenters/instance_presenter_spec.rb
@@ -108,8 +108,8 @@ describe InstancePresenter do
         end
       end
 
-      it 'defaults to the core glitch-soc repo URL' do
-        expect(instance_presenter.source_url).to eq('https://github.com/glitch-soc/mastodon')
+      it 'defaults to the core chuckya repo URL' do
+        expect(instance_presenter.source_url).to eq('https://github.com/TheEssem/mastodon')
       end
     end
   end
