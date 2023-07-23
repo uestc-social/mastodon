ebc46a0ad2f2c7708ff8d71bb4e5a67f9eb6d1be
TykeTC <dev@uestc.social>
2023-07-23 21:17:35 +0000

    [us] expose basic APIs

--- app/controllers/api/v1/custom_emojis_controller.rb
+++ app/controllers/api/v1/custom_emojis_controller.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class Api::V1::CustomEmojisController < Api::BaseController
-  vary_by '', unless: :disallow_unauthenticated_api_access?
+  vary_by ''
 
   def index
-    cache_even_if_authenticated! unless disallow_unauthenticated_api_access?
+    cache_even_if_authenticated!
     render_with_cache(each_serializer: REST::CustomEmojiSerializer) { CustomEmoji.listed.includes(:category) }
   end
 end
