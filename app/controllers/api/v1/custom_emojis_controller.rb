43a1b18ed90f0842ce4d385da74dc8d6dc7ce00e
TykeTC <dev@uestc.social>
2023-08-12 19:01:28 +0200

    fix not opening /api/v1/custom_emojis correctly

--- app/controllers/api/v1/custom_emojis_controller.rb
+++ app/controllers/api/v1/custom_emojis_controller.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 class Api::V1::CustomEmojisController < Api::BaseController
+  skip_before_action :require_authenticated_user!
+
   vary_by ''
+
+  # Override `current_user` to avoid reading session cookies unless in whitelist mode
+  def current_user
+    super if limited_federation_mode?
+  end
 
   def index
     cache_even_if_authenticated!
