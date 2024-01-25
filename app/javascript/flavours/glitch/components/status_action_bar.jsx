fb569a61d5966b5b5cfb19324aa48a9ba8ec128e
TykeTC <dev@uestc.social>
2024-01-25 06:13:26 +0100

    show number of replies, reblogs and favourites
    Co-authored-by: OmmyZhang <tdxdxoz@gmail.com>

--- app/javascript/flavours/glitch/components/status_action_bar.jsx
+++ app/javascript/flavours/glitch/components/status_action_bar.jsx
@@ -330,11 +330,11 @@ class StatusActionBar extends ImmutablePureComponent {
           icon={replyIcon}
           iconComponent={replyIconComponent}
           onClick={this.handleReplyClick}
-          counter={showReplyCount ? status.get('replies_count') : undefined}
-          obfuscateCount
+          counter={status.get('replies_count')}
+          obfuscateCount={showReplyCount}
         />
-        <IconButton className={classNames('status__action-bar-button', { reblogPrivate })} disabled={!publicStatus && !reblogPrivate} active={status.get('reblogged')} title={reblogTitle} icon={reblogIcon} iconComponent={reblogIconComponent} onClick={this.handleReblogClick} counter={withCounters ? status.get('reblogs_count') : undefined} />
-        <IconButton className='status__action-bar-button favorite-icon' animate active={status.get('favourited')} title={intl.formatMessage(messages.favourite)} icon='favorite' iconComponent={status.get('favourited') ? FavoriteIcon : FavoriteBorderIcon} onClick={this.handleFavouriteClick} counter={withCounters ? status.get('favourites_count') : undefined} />
+        <IconButton className={classNames('status__action-bar-button', { reblogPrivate })} disabled={!publicStatus && !reblogPrivate} active={status.get('reblogged')} title={reblogTitle} icon={reblogIcon} iconComponent={reblogIconComponent} onClick={this.handleReblogClick} counter={status.get('favourites_count')} obfuscateCount={showReplyCount} />
+        <IconButton className='status__action-bar-button favorite-icon' animate active={status.get('favourited')} title={intl.formatMessage(messages.favourite)} icon='favorite' iconComponent={status.get('favourited') ? FavoriteIcon : FavoriteBorderIcon} onClick={this.handleFavouriteClick} counter={status.get('favourites_count')} obfuscateCount={showReplyCount}/>
         <IconButton className='status__action-bar-button bookmark-icon' disabled={!signedIn} active={status.get('bookmarked')} title={intl.formatMessage(messages.bookmark)} icon='bookmark' iconComponent={status.get('bookmarked') ? BookmarkIcon : BookmarkBorderIcon} onClick={this.handleBookmarkClick} />
 
         {filterButton}
