74c2f188ab2c6bd4229910a72a6eae965f081d3d
TykeTC <dev@uestc.social>
2024-01-25 09:58:13 +0100

    closed-social brandings
    Co-authored-by: OmmyZhang <tdxdxoz@gmail.com>

--- app/javascript/flavours/glitch/features/about/index.jsx
+++ app/javascript/flavours/glitch/features/about/index.jsx
@@ -124,7 +124,7 @@ class About extends PureComponent {
           <div className='about__header'>
             <ServerHeroImage blurhash={server.getIn(['thumbnail', 'blurhash'])} src={server.getIn(['thumbnail', 'url'])} srcSet={server.getIn(['thumbnail', 'versions'])?.map((value, key) => `${value} ${key.replace('@', '')}`).join(', ')} className='about__header__hero' />
             <h1>{isLoading ? <Skeleton width='10ch' /> : server.get('domain')}</h1>
-            <p><FormattedMessage id='about.powered_by' defaultMessage='Decentralized social media powered by {mastodon}' values={{ mastodon: <a href='https://joinmastodon.org' className='about__mail' target='_blank'>Mastodon</a> }} /></p>
+            <p><a href='https://closed.social' className='about__mail' target='_blank'>闭社</a>成员站点</p>
           </div>
 
           <div className='about__meta'>
@@ -210,7 +210,7 @@ class About extends PureComponent {
           <LinkFooter />
 
           <div className='about__footer'>
-            <p><FormattedMessage id='about.fork_disclaimer' defaultMessage='Glitch-soc is free open source software forked from Mastodon.' /></p>
+            <p><FormattedMessage id='about.fork_disclaimer' defaultMessage='Closed-social is free open source software forked from Mastodon.' /></p>
             <p><FormattedMessage id='about.disclaimer' defaultMessage='Mastodon is free, open-source software, and a trademark of Mastodon gGmbH.' /></p>
           </div>
         </div>
